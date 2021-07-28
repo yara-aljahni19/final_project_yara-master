26ee8a9d55b043ad744f49872b1057fb11561635
yara-aljahni19 <yaljahni@stu.kau.edu.sa>
2021-07-28 18:56:37 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/final_project_yara/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.final_project_yara
+
+import android.content.Context
+import android.os.Bundle
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+class MainActivity: FlutterActivity() {
+
+}
